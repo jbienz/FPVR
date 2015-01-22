64a06752c607d53eebeffac1fde85c7373d99581
jbienz <jbienz@hotmail.com>
2015-01-21 21:12:39 -0600

    Organized FPV lib a bit better.

---
+++ Lib/FPVR/Interfaces/ISpeedInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FPVR
+{
+    /// <summary>
+    /// Provides speed information.
+    /// </summary>
+    public interface ISpeedInfo
+    {
+        /// <summary>
+        /// The speed in meters per second.
+        /// </summary>
+        double Speed { get; }
+    }
+}
